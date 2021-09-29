bdd83d0b48c5c3ec7069b218d569b63d71d69a1e
Firas <70592307+fj17x@users.noreply.github.com>
2021-09-29 18:07:03 +0530

    Update Llist06.cpp

--- Linked list/Llist06.cpp
+++ Linked list/Llist06.cpp
@@ -3,17 +3,24 @@ using namespace std;
 
 // Display only odd numbers in nodes
 
+#include <iostream>
+using namespace std;
+
+// Display only even numbers in nodes
+
 class Node
 {
     int data;
     Node *next;
+    static int count;
 
 public:
     void enter(Node *x = NULL)
     {
-        cout << "\n Enter data: ";
+        cout << "Enter data of node " << count << ": ";
         cin >> data;
         next = x;
+        count++;
     }
 
     Node *gnext()
@@ -32,6 +39,49 @@ public:
     }
 
 } * head;
+
+int Node::count = 1;
+
+void createNode(int n)
+{
+    head = new Node;
+    head->enter();
+
+    Node *temp = new Node;
+    temp = head;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        Node *newnode = new Node;
+        newnode->enter();
+        temp->cnext(newnode);
+        temp = temp->gnext();
+    }
+}
+
+void etravese()
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (temp->gdata() % 2 == 0)
+            cout << "\n " << temp->gdata() << endl;
+        temp = temp->gnext();
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter number of nodes to create: ";
+    cin >> n;
+    createNode(n);
+
+    cout << "\nNow displaying even data through the nodes...\n";
+    etravese();
+}
+
 
 void createNode(int n)
 {
